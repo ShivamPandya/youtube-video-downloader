7a9356034c7766a64d5bd81a4ec30c3dd301fb95
shivampandya <sampandya.sp@gmail.com>
2017-06-29 00:55:40 +0530

    all ready

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # youtube-video-downloader
+
+This is simple python script, written to download any music* video from youtube.
+
+I've used youtube-dl and beautifulsoup, additionally I've used urllib to get the links and to request the search.
+
+Program requests the YouTube search and selects the top result(as for most youtube video the first search is the most relevant)
---
+++ vid_download.py
@@ -0,0 +1,26 @@
+
+# coding: utf-8
+
+# In[43]:
+
+
+import urllib.request as urlreq
+import youtube_dl as ydl
+from bs4 import BeautifulSoup as bs
+from urllib.parse import urlparse
+
+links = []
+song1 = input("Enter song name with singer (Ex: hello by adele): ")
+song = song1.replace(" ", "+")
+
+search = "https://www.youtube.com/results?search_query=" + song
+urlparse(search)
+
+#print(search)
+
+x = urlreq.urlopen(search)
+soup= bs(x.read(),"html5lib")
+for vid in soup.findAll(attrs={'class':'yt-uix-tile-link'}):
+    links.append('https://www.youtube.com' + vid['href'])
+ydl.YoutubeDL().download([links[0]])
+
